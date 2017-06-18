87f4f83dd43267e64debe2cbdfea34b393963bff
Alan Drees <alandrees@theselves.com>
2017-06-18 10:10:48 -0400

    added documentation to the ImgSlack application

--- ImgSlack.py
+++ ImgSlack.py
@@ -1,3 +1,12 @@
+"""
+Copyright 2017 Pirate Hour Productions
+
+Author: Alan Drees
+
+Purpose: Flask-enabled http endpoints for recieving events from slack
+and adding the image to queue job
+"""
+
 import yaml
 from flask import Flask
 from flask import request
@@ -10,13 +19,29 @@ app_token = 'oeZ8mFbswzdGBnLpEbcvxT4v'
 
 @app.route('/status/', methods=['GET'])
 def status():
-    stalk = greenstalk.Client(host='127.0.0.1', port=11300)
+    """
+    Route Function for handling the queue status page
+
+    @param None
+
+    @returns Json Response containing the greenstalk stats
+    """
+    stalk = greenstalk.Client()
     stats = stalk.stats()
     return jsonify(stats)
 
 
-@app.route('/image', methods=['POST'])
-def image_event():
+@app.route('/image/<team>', methods=['POST'])
+def image_event(team):
+    """
+    Route Function for handling the image event, and push it to the queue
+
+    @param team (string) team to which this image belongs
+
+    @returns Json Response containing the keypair {'jerb' : jobid}
+    """
+    image_id = request.json['event']['file']['id']
+
     if app_token == request.json['token']:
             stalk = greenstalk.Client()
             jobid = stalk.put(image_id)
